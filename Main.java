a8b7d3c9065c84b5138a7396084ce887621b7f4f
Okemwa Brian <okemwabrian135@gmail.com>
2026-01-12 22:38:34 +0300

    variables

--- Main.java
+++ Main.java
@@ -1,12 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Welcome to the Java programming language.");
-        System.out.println("you can use gdoyuble to generate code snippets.");
-        System.out.println("Enjoy coding!");
-        System.out.println("Goodbye!");//you cant use single quotes to write a string in java
-        System.out.println("Have a great day!");
-        System.out.print("Stay safe and happy coding!");//when using print instead of println, the cursor stays on the same line
-        System.out.println("See you next time!");
+        // System.out.println("Hello, World!");
+        // System.out.println("Welcome to the Java programming language.");
+        // System.out.println("you can use gdoyuble to generate code snippets.");
+        // System.out.println("Enjoy coding!");
+        // System.out.println("Goodbye!");//you cant use single quotes to write a string in java
+        // System.out.println("Have a great day!");
+        // System.out.print("Stay safe and happy coding!");//when using print instead of println, the cursor stays on the same line
+        // System.out.println("See you next time!");
+        // System.out.println(342 + 125); //this will print the sum of the two numbers;
+        // System.out.println(200  ); //this will print out the number 200
+
+        //variables in java
+        int myNumber   =100;
+        String name = "Okemwa";
+        int age = 24;
+        System.out.println("My name is " + name + " and am " + age + " years old.");
+        System.out.println("My number is " + myNumber);
+
+
     }
-}
+} 
