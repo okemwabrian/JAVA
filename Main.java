e5aaab29ef3483179e62efdf37610508c1c82452
Okemwa Brian <okemwabrian135@gmail.com>
2026-01-12 22:27:30 +0300

    println

--- Main.java
+++ Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to the Java programming language.");
+        System.out.println("you can use gdoyuble to generate code snippets.");
+        System.out.println("Enjoy coding!");
+        System.out.println("Goodbye!");//you cant use single quotes to write a string in java
+        System.out.println("Have a great day!");
+        System.out.print("Stay safe and happy coding!");//when using print instead of println, the cursor stays on the same line
+        System.out.println("See you next time!");
     }
 }
