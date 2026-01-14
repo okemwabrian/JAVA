0e6f86a4ae743b5f4fd75630d4fdaaa8e2de4698
Okemwa Brian <okemwabrian135@gmail.com>
2026-01-14 18:53:59 +0300

    JavaSwitches and modified statements

--- JavaStatements.java
+++ JavaStatements.java
@@ -18,5 +18,92 @@ public class JavaStatements {
         for (int i = 0; i < 5; i++) {
             System.out.println("Iteration: " + i);
         }
+
+// Switch statement
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            default:
+                System.out.println("Another day");
+                break;
+        }
+////Java Short Hand If...Else (Ternary Operator)
+        int number = 8;
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println("The number is: " + result);
+
+
+
+        // While loop statement
+        int count = 0;
+        while (count < 3) {
+            System.out.println("Count is: " + count);
+            count++;
+
+            //Nested Ternary
+            int score = 85;
+            String grade = (score >= 90) ? "A" :
+                    (score >= 80) ? "B" :
+                            (score >= 70) ? "C" :
+                                    (score >= 60) ? "D" : "F";
+            System.out.println("The grade is: " + grade);
+
+        }
+        //Java Nested If
+        int age = 25;
+        if (age >= 18) {
+            if (age < 65) {
+                System.out.println("Adult");
+            } else {
+                System.out.println("Senior Citizen");
+            }
+        } else {
+            System.out.println("Minor");
+        }
+        System.out.println("Done");
+        System.out.println("This is the end of the program.");
+        System.out.println("Goodbye!");
+
+
+
+
+        //Java If ... Else Examples
+        int temperature = 30;
+        if (temperature > 30) {
+            System.out.println("It's a hot day.");
+        } else if (temperature >= 20) {
+            System.out.println("It's a warm day.");
+        } else {
+            System.out.println("It's a cold day.");
+        }
+
+
+
+
+        //Countdown Example
+        int countdown = 5;
+        while (countdown > 0) {
+            System.out.println("Countdown: " + countdown);
+            countdown--;
+        }
+
+                //Countdown Example to happy new year
+                int year = 2024;
+                for (int i = 10; i > 0; i--) {
+            System.out.println("Countdown: " + i);
+        }
+        System.out.println("Happy New Year " + year + "!");
+        
+
+
+
     }
 }
---
+++ JavaSwitch.java
@@ -0,0 +1,84 @@
+public class JavaSwitch {
+    public static void main(String[] args) {
+        int day = 3;
+        String dayName;
+
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+                break;
+        }
+
+        System.out.println("The day is: " + dayName);
+
+
+                //using default 
+        int month = 13;
+        String monthName;
+        switch (month) {
+            case 1:
+                monthName = "January";
+                break;
+            case 2:
+                monthName = "February";
+                break;
+            case 3:
+                monthName = "March";
+                break;
+            case 4:
+                monthName = "April";
+                break;
+            case 5:
+                monthName = "May";
+                break;
+            case 6:
+                monthName = "June";
+                break;
+            case 7:
+                monthName = "July";
+                break;
+            case 8:
+                monthName = "August";
+                break;
+            case 9:
+                monthName = "September";
+                break;
+            case 10:
+                monthName = "October";
+                break;
+            case 11:
+                monthName = "November";
+                break;
+            case 12:
+                monthName = "December";
+                break;
+            default:
+                monthName = "Invalid month";
+                break;
+        }
+        System.out.println("The month is: " + monthName);
+
+
+    }
+}
